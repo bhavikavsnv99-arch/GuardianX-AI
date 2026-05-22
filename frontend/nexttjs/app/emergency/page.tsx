7e393c278a4f401b8cc1134b3dbe318a5a875e3f
Bhavika Vaishnav <bhavikavsnv99@gmail.com>
2026-05-22 19:42:41 +0530

    frontend updated

---
+++ frontend/nexttjs/app/emergency/page.tsx
@@ -0,0 +1,7 @@
+export default function Emergency() {
+  return (
+    <div>
+      <h1>Emergency Page</h1>
+    </div>
+  );
+}
--- frontend/nexttjs/app/page.tsx
+++ frontend/nexttjs/app/page.tsx
@@ -1,8 +1,21 @@
+import Navbar from "../components/navbar";
+
 export default function Home() {
   return (
-    <div>
-      <h1>GuardianX AI</h1>
-      <p>Emergency & Safety Assistant</p>
+    <div className="p-10">
+      <Navbar />
+
+      <h1 className="text-4xl font-bold mt-10">
+        GuardianX AI
+      </h1>
+
+      <p className="mt-4">
+        AI Emergency & Safety Assistant
+      </p>
+
+      <button className="bg-red-500 text-white px-5 py-3 rounded mt-6">
+        Send Emergency Alert
+      </button>
     </div>
   );
 }
